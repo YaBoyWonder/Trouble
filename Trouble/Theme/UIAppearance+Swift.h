2879eb4c3d97fab2590a0fa2215f53d6e052eb9c
Rahul Chalumuri <rahulchalumuri54@gmail.com>
2017-03-07 05:27:11 -0500

    Update UIAppearance+Swift.h

--- Trouble/Theme/UIAppearance+Swift.h
+++ Trouble/Theme/UIAppearance+Swift.h
@@ -10,7 +10,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface UIBarItem (UIBarItemAppearance_Swift)
 
-  + (instancetype) inside:(Class<UIAppearanceContainer>)containerClass;
+   + (instancetype) inside:(Class<UIAppearanceContainer>)containerClass;
 
 NS_ASSUME_NONNULL_END
 
