2c39d4be7a450274506af8c813da7ec2fa81cf99
Rahul Chalumuri <rahulchalumuri54@gmail.com>
2017-03-11 08:13:07 -0500

    Update AppDelegate.swift

--- Trouble/AppDelegate.swift
+++ Trouble/AppDelegate.swift
@@ -7,7 +7,7 @@ import UIKit
 
     
       func applic.(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject : AnyObject]?) -> Bool {
-         self.window!.backgroundColor = UIColor.whiteColor()
+        self.window!.backgroundColor = UIColor.whiteColor()
         return true
     }
 
