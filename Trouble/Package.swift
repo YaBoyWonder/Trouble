b3238a99a9cfea56240bd2ce7ac2f12d648d319c
Rahul Chalumuri <rahulchalumuri54@gmail.com>
2017-03-12 22:05:24 -0400

    Create Package.swift

---
+++ Trouble/Package.swift
@@ -0,0 +1,18 @@
+import PackageDescription
+
+let package = Package(
+name: "Trouble",
+exclude: [
+    "{}"
+],
+targets: [
+    Target(
+        name: "Trouble"
+    ),
+    Target(
+        name: "{}Tests",
+        dependencies: [
+            .Target(name: "{}")
+        ]
+    )
+]
