8a1e90accf9e2d3da52315e1356e86ee73b9422b
Rahul Chalumuri <rahulchalumuri54@gmail.com>
2017-03-16 21:10:14 -0400

    Update UIAppearance+Swift.h

--- Trouble/Theme/UIAppearance+Swift.h
+++ Trouble/Theme/UIAppearance+Swift.h
@@ -4,7 +4,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface UIView (UIViewAppearance_Swift)
 
- + (instancetype) inside:(Class<UIAppearanceContainer>)containerClass;
+  + (instancetype) inside:(Class<UIAppearanceContainer>)containerClass;
 
 @end
 
